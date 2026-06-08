44a19d45c23bc9288d8d8727ed24212bbde3b8dc
Konstantin Merkel <konstantin.merkel@tutanota.de>
2026-06-08 20:34:57 +0200

    first working iteration maximisation rework

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import Gio from 'gi://Gio';
 import { TilingController } from './lib/controller.js';
 import { SignalListener } from './lib/signals.js';
 import { SettingsManager } from './lib/settings.js';
@@ -21,6 +22,20 @@ export default class WorkflowTilingExtension extends Extension {
         
         this._isActive = false;
         this._wasSuspended = false;
+
+        this._isActive = false;
+        this._wasSuspended = false;
+
+        try {
+            this._wmSettings = new Gio.Settings({ schema_id: 'org.gnome.desktop.wm.preferences' });
+            let currentLayout = this._wmSettings.get_string('button-layout');
+            if (currentLayout.includes('maximize')) {
+                let newLayout = currentLayout.replace(/maximize,?/g, '').replace(/,maximize/g, '');
+                this._wmSettings.set_string('button-layout', newLayout);
+            }
+        } catch (e) {
+            Logger.warn('Failed to hide maximize button', e);
+        }
 
         this._settings.onSettingsChanged = () => {
             if (this._applyCustomLayouts()) {
@@ -84,5 +99,25 @@ export default class WorkflowTilingExtension extends Extension {
         this._controller = null;
         this._isActive = false;
         this._wasSuspended = false;
+
+        if (this._wmSettings) {
+            try {
+                let layout = this._wmSettings.get_string('button-layout');
+                if (!layout.includes('maximize')) {
+                    if (layout.includes('minimize,close')) {
+                        layout = layout.replace('minimize,close', 'minimize,maximize,close');
+                    } else if (layout.includes('close')) {
+                        layout = layout.replace('close', 'maximize,close');
+                    } else {
+                        layout += ',maximize';
+                    }
+                    this._wmSettings.set_string('button-layout', layout);
+                    Gio.Settings.sync();
+                }
+            } catch (e) {
+                Logger.error('Failed to restore maximize button', e);
+            }
+            this._wmSettings = null;
+        }
     }
 }
